d4b1e6d74f7d33b409b1e526dd76cf08d130adf6
Luis gonzalez <luisworker2010@gmail.com>
2024-04-04 10:55:54 +0200

    reading from kafka

--- src/main/java/org/example/Consumer.java
+++ src/main/java/org/example/Consumer.java
@@ -1,28 +1,60 @@
 package org.example;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.KafkaException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import java.util.*;
 
 public class Consumer {
-    public static void main(String[] args) {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "test");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Collections.singletonList("develop"));
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> record : records)
-                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+
+    private KafkaConsumer<String, String> kafkaConsumer;
+
+    private Consumer() {
+        try {
+            Properties conf = new Properties();
+            conf.load(new FileReader("src/main/resources/consumer.properties"));
+            this.kafkaConsumer = new KafkaConsumer(conf);
+        } catch (IOException ioe) {
+            log.error(ioe.getMessage());
         }
     }
+
+    public void start() {
+        int count = 0;
+        do {
+            try {
+                kafkaConsumer.subscribe(Collections.singletonList(TOPIC));
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(20));
+                records.forEach(r -> {
+                    String msg = String.format(
+                            "offset %s, partition %s, key %s, value %s", r.offset(), r.partition(), r.key(), r.value()
+                    );
+                    log.info(msg);
+                });
+
+            } catch (KafkaException e) {
+                log.error(e.getMessage());
+                this.close();
+            }
+            count ++;
+        } while (count <= 100);
+    }
+
+    public void close() {
+        this.kafkaConsumer.close();;
+    }
+
+    public static Consumer getInstance() {
+        return (Objects.nonNull(consumer)) ? consumer : new Consumer();
+    }
+
+    private static Consumer consumer;
+    private static final String TOPIC = "develop";
+    private static final Logger log = LogManager.getLogger(Consumer.class);
 }
